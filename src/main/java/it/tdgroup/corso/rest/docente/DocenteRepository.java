700fc0c781828717267b49a2b6b102a0c63a5d56
Tony-jdl <81505820+Tony-jdl@users.noreply.github.com>
2021-11-13 12:37:11 +0100

    Delete src directory

--- src/main/java/it/tdgroup/corso/rest/docente/DocenteRepository.java
+++
@@ -1,9 +0,0 @@
-package it.tdgroup.corso.rest.docente;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository(value = "docenteRepository")
-public interface DocenteRepository extends MongoRepository<Docente, String> {
-
-}
